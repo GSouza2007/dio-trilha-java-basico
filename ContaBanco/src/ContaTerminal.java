1a02c969bd52fb3df965fbd2de2a7106b613afba
GSouza2007 <ouzasantos1505@gmail.com>
2026-05-26 17:39:18 -0300

    Implementa funcionalidade de criação de conta bancária com entrada de dados do usuário

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Digite o número da conta: ");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Digite o nome do titular da conta: ");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Digite o nome da agencia bancaria: ");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o seu saldo atual: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta +" e seu saldo "+saldo+" já está disponível para saque");
     }
 }
